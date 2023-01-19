a912874689f4abf7b2db9d5d8486f32dff388c6f
Steplton7 <107725240+Steplton7@users.noreply.github.com>
2023-01-20 00:14:12 +0300

    парметры, валидация, body

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
-from schemas import Book
+from typing import List
+from fastapi import FastAPI, Query, Path, Body
+from schemas import Book, Author
 
 app = FastAPI()
 
@@ -9,16 +10,38 @@ def home():
     return {"key":"Hello"}
 
 
-@app.get('/{pk}') # 127.0.0.1:8000/2?q=re
+@app.get('/test/{pk}') # 127.0.0.1:8000/2?q=re
 def get_item(pk: int, q: str = None):
+    """ Пример работы url. """
     return {"key": pk, "q": q}
 
 
 @app.get('user/{pk}/items/{item}')
 def get_user_item(pk: int, item: str):
+    """ Пример работы url. """
     return {"user": pk, "item": item}
 
 
+
 @app.post('/book')
-def create_book(item: Book):
-    return item
+def create_book(item: Book, author: Author, quantity: int = Body(...)):
+    """ Модели. Указание аргументов которые входят только в тело запроса, а не url."""
+    return {"item": item, "author": author, "quantity": quantity}
+
+
+@app.post('/author')
+def create_author(author: Author = Body(..., embed=True)):
+    """ Для того что бы в json был ключ Author."""
+    return {"author": author}
+
+
+@app.get('/book') # ... показывает что параметр обязательный
+def gte_book(q: List[str] = Query(["1","2"], description='Search book')):
+    """ Парметры."""
+    return q
+
+
+@app.get('/book/{pk}') # gt -min. le - max.
+def get_single(pk: int = Path(..., gt=1, le=20), pages: int = Query(None, gt=10, le=500)):
+    """ Функция получения книги по id."""
+    return {"pk": pk}
